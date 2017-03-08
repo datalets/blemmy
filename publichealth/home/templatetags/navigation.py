0f27d97d648f4f0a048e8d10f649714ef7ae374a
Oleg Lavrovsky <oleg@utou.ch>
2017-03-08 22:33:40 +0100

    Header menu

--- publichealth/home/templatetags/navigation.py
+++ publichealth/home/templatetags/navigation.py
@@ -12,3 +12,37 @@ def language_switcher(context):
         'currentlangcode': translation.get_language(),
         'request': context['request'],
     }
+
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
+    return context['request'].site.root_page
+
+def has_menu_children(page):
+    return page.get_children().live().in_menu().exists()
+
+# Retrieves the top menu items
+@register.inclusion_tag('tags/top_menu.html', takes_context=True)
+def top_menu(context, parent, calling_page=None):
+    menuitems = parent.get_children().live().in_menu()
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
+        'request': context['request'],
+    }
+
+# Retrieves the children of the top menu items for the drop downs
+@register.inclusion_tag('tags/top_menu_children.html', takes_context=True)
+def top_menu_children(context, parent):
+    menuitems_children = parent.get_children()
+    menuitems_children = menuitems_children.live().in_menu()
+    return {
+        'parent': parent,
+        'menuitems_children': menuitems_children,
+        'request': context['request'],
+    }
