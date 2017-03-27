f48aaabf09121d9c929c45adf746a4b304ee1b0e
Oleg Lavrovsky <oleg@utou.ch>
2017-03-28 00:43:55 +0200

    Db config

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 # web: uwsgi --http :$PORT --module=publichealth.wsgi:application --master --offload-threads 3
-# web: gunicorn publichealth.wsgi -b 0.0.0.0:$PORT -w 5 --log-file=-
-web: python manage.py runserver $PORT
+web: gunicorn publichealth.wsgi:application -b 0.0.0.0:$PORT -w 5 --log-file=-
 init: python manage.py migrate
--- publichealth/settings/base.py
+++ publichealth/settings/base.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
@@ -95,10 +96,7 @@ WSGI_APPLICATION = 'publichealth.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'publichealth',
-    }
+    'default': dj_database_url.config()
 }
 
 
