9f082f3ae66f5056dbfb81a2f6731379f6409ced
Oleg Lavrovsky <oleg@utou.ch>
2017-04-06 09:42:47 +0200

    Production settings

--- publichealth/settings/production.py
+++ publichealth/settings/production.py
@@ -134,7 +134,7 @@ if REDIS_LOCATION is not None:
 if 'ELASTICSEARCH_URL' in env:
     WAGTAILSEARCH_BACKENDS = {
         'default': {
-            'BACKEND': 'wagtail.wagtailsearch.backends.elasticsearch.ElasticSearch',
+            'BACKEND': 'wagtail.wagtailsearch.backends.elasticsearch5',
             'URLS': [env['ELASTICSEARCH_URL']],
             'INDEX': APP_NAME,
             'ATOMIC_REBUILD': True,
