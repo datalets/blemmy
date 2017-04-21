456ae79ea55927e3d5e28096315a1cd4c7a85c6a
Oleg Lavrovsky <oleg@utou.ch>
2017-04-21 15:17:53 +0200

    Datalets feedback form

--- publichealth/home/models/settings.py
+++ publichealth/home/models/settings.py
@@ -9,28 +9,33 @@ from wagtail.contrib.settings.models import BaseSetting, register_setting
 
 @register_setting
 class DataletsSettings(BaseSetting):
-    feedback_question = models.TextBlock(
-        help_text='Send us a question')
+    feedback_question = models.TextField(
+        help_text='Send us a question', blank=True)
     feedback_status = models.IntegerField(
         choices=(
-            (1, _':-('),
-            (2, _':-|'),
-            (3, _':-)'),
-            (4, _':-D'),
-        ), help_text='How are you enjoying Wagtail?'
+            (1, ':-('),
+            (2, ':-|'),
+            (3, ':-)'),
+            (4, ':-D'),
+        ), blank=True, null=True,
+        help_text='How are you enjoying Wagtail?'
     )
-    feedback_comment = models.TextBlock(
-        help_text='Any general feedback')
+    feedback_comment = models.TextField(
+        help_text='Any general feedback', blank=True)
     class Meta:
         verbose_name = 'Datalets'
 
 @receiver(pre_save, sender=DataletsSettings)
-def handle_save_settings(sender, **kwargs):
-    send_mail("Response from Wagtail",
-        "%d\n--\n%s\n--\n%s" % (
-            sender.feedback_status,
-            sender.feedback_question,
-            sender.feedback_comment,
-        ), "wagtail@datalets.ch",
-        [ "support@datalets.ch" ]
-    )
+def handle_save_settings(sender, instance, *args, **kwargs):
+    if instance.feedback_status is not None:
+        send_mail("Response from Wagtail",
+            "%s\n--\n%s\n--\n%s" % (
+                str(instance.feedback_status),
+                instance.feedback_question,
+                instance.feedback_comment,
+            ), "wagtail@datalets.ch",
+            [ "support@datalets.ch" ]
+        )
+        instance.feedback_status = None
+        instance.feedback_question = ""
+        instance.feedback_comment = ""
