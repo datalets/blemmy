2d1dd126d838f7bfc7a6c3485278cfe5ee8830c6
khashashin <khashashin@gmail.com>
2017-08-31 16:06:44 +0200

    endpoints output issue fixed

--- cultinadb/endpoints.py
+++ cultinadb/endpoints.py
@@ -1,30 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from wagtail.api.v2.endpoints import BaseAPIEndpoint
-from wagtail.api.v2.filters import FieldsFilter, OrderingFilter, SearchFilter
-from wagtail.api.v2.serializers import BaseSerializer
 
 from .models import Week, Ingredient, Menu
 
 class WeekAPIEndpoint(BaseAPIEndpoint):
-    base_serializer_class = BaseSerializer
-    filter_backends = [FieldsFilter, OrderingFilter, SearchFilter]
-    name = 'week'
     model = Week
+    listing_default_fields = BaseAPIEndpoint.listing_default_fields + ['year', 'week']
+
 
 class IngredientsAPIEndpoint(BaseAPIEndpoint):
-    base_serializer_class = BaseSerializer
-    filter_backends = [FieldsFilter, OrderingFilter, SearchFilter]
-    name = 'ingredients'
     model = Ingredient
-
-class MenuAPIEndpoint(BaseAPIEndpoint):
-    base_serializer_class = BaseSerializer
-    filter_backends = [FieldsFilter, OrderingFilter, SearchFilter]
-    name = 'menu'
-    model = Menu
 
 def register_endpoints(api_router):
     api_router.register_endpoint('week', WeekAPIEndpoint)
     api_router.register_endpoint('ingredients', IngredientsAPIEndpoint)
-    api_router.register_endpoint('menu', MenuAPIEndpoint)
